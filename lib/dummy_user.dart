8b07add34c5b4c6b69fabb83c5aae83906d20e73
webadib <webadib@gmail.com>
2025-03-10 23:20:05 +0600

    getting data from api

--- lib/dummy_user.dart
+++ lib/dummy_user.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class DummyUser extends StatefulWidget {
   const DummyUser({super.key});
@@ -8,6 +11,24 @@ class DummyUser extends StatefulWidget {
 }
 
 class _DummyUserState extends State<DummyUser> {
+  List users = [];
+  Future<void> fetchUsers() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    if (response.statusCode == 200) {
+      users = jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to fetch users');
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchUsers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +36,7 @@ class _DummyUserState extends State<DummyUser> {
         title: Text('Dummy User'),
       ),
       body: ListView.builder(
-        itemCount: 10,
+        itemCount: users.length,
         itemBuilder: (context, index) {
           return Card(
             child: ListTile(
@@ -23,8 +44,16 @@ class _DummyUserState extends State<DummyUser> {
                 child: Icon(Icons.person),
                 backgroundColor: Colors.blue.shade300,
               ),
-              title: Text('User $index'),
-              subtitle: Text('This is a dummy user'),
+              title: Text(users[index]['name']),
+              subtitle: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Email: ${users[index]['email']}'),
+                  Text('Phone: ${users[index]['phone']}'),
+                  Text('Website: ${users[index]['website']}'),
+                ],
+              ),
             ),
           );
         },
